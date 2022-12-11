677c8cccd0f9e5884694f1fbaba0f62614b14820
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-12-11 10:52:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
-echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
+echo "Cloning Repo, Please Wait..."
+git clone -b main https://github.com/Dkmovie/Mdisk-Search-Bot-Pro.git /Mdisk-Search-Bot-Pro
+cd /Mdisk-Search-Bot-Pro
+echo "Installing Requirements..."
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 bot.py
